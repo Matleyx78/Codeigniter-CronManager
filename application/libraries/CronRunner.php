aef1e3c9f50fd7222ce34eff9a42a4fdb7c94a0f
Matleyx78 <matmat78@hotmail.it>
2018-10-18 15:10:08 +0200

    Update CronRunner.php

--- application/libraries/CronRunner.php
+++ application/libraries/CronRunner.php
@@ -2,30 +2,36 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class CronRunner
-{
- private $CI;
+    {
+        private $CI;
 
- public function __construct()
- {
-    $this->CI =& get_instance();
- }
+    public function __construct()
+        {
+            $this->CI =& get_instance();
+            $this->CI->load->database();
+        }
 
- private function calculateNextRun($obj)
- {
-    return (time() + $obj->interval_sec);
- }
+    private function calculateNextRun($obj)
+        {
+            return (time() + $obj->cicj_interval_sec);
+        }
 
- public function run()
- {
-   $query = $this->CI->db->where('is_active', 1)->where('now() >= next_run_at OR next_run_at IS NULL', '', false)->from('cron')->get();
-   if ($query->num_rows() > 0) {
-     $env = getenv('CI_ENV');
-       foreach ($query->result() as $row) {
-         $cmd = "export CI_ENV={$env} && {$row->command}";
-         $this->CI->db->set('next_run_at', 'FROM_UNIXTIME('.$this->calculateNextRun($row).')', false)->where('id', $row->id)->update('cron');
-         $output = shell_exec($cmd);
-         $this->CI->db->set('last_run_at', 'now()', false)->where('id', $row->id)->update('cron');
-       }
-    } 
-  }
+    public function run()
+        {
+            $query = $this->CI->db->where('cicj_is_active', 1)->where('now() >= cicj_next_run_at OR cicj_next_run_at IS NULL', '', false)->from('ci_cron_job')->get();
+            
+            if ($query->num_rows() > 0)
+                {
+                $env = getenv('CI_ENV');
+                
+                foreach ($query->result() as $row)
+                    {
+                        $cmd = $row->cicj_command;
+                        //$cmd = "export CI_ENV={$env} && {$row->cicj_command}";
+                        $this->CI->db->set('cicj_next_run_at', 'FROM_UNIXTIME('.$this->calculateNextRun($row).')', false)->where('id_ci_cj', $row->id_ci_cj)->update('ci_cron_job');
+                        $output = shell_exec($cmd);
+                        $this->CI->db->set('cicj_last_run_at', 'now()', false)->where('id_ci_cj', $row->id_ci_cj)->update('ci_cron_job');
+                    }
+                } 
+        }
 }
