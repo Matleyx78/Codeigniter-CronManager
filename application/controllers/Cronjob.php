a26b7658363bd502adbb5e5508bdb1dbcc4c6a36
Matleyx78 <matmat78@hotmail.it>
2018-10-18 15:09:44 +0200

    Update Cronjob.php

--- application/controllers/Cronjob.php
+++ application/controllers/Cronjob.php
@@ -1,20 +1,21 @@
 <?php if (! defined('BASEPATH')) { exit('No direct script access allowed');}
 
 
-class Cron extends MY_Controller
-  {
-     public function __construct()
-       {
-         parent::__construct();
-         if (!$this->input->is_cli_request()) {
-         show_error('Direct access is not allowed');
-         }
-       }
+class Cronjob extends CI_Controller
+{
+    public function __construct()
+        {
+            parent::__construct();
+        }
 
- public function run()
-     {
+    public function run()
+        {
+        if (!$this->input->is_cli_request())
+            {
+                show_error('Direct access is not allowed');
+            }
         $this->load->library('CronRunner');
         $cron = new CronRunner();
         $cron->run();
-     }
+        }
 }
