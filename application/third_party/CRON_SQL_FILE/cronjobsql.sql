e7ade01991cdefae70b0120bddcea0c275c84dc3
Matleyx78 <matmat78@hotmail.it>
2018-10-18 15:10:40 +0200

    Update cronjobsql.sql

--- application/third_party/CRON_SQL_FILE/cronjobsql.sql
+++ application/third_party/CRON_SQL_FILE/cronjobsql.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `cron` (
-`id` int(5) NOT NULL,
-`name` varchar(100) DEFAULT NULL,
-`command` varchar(255) NOT NULL,
-`interval_sec` int(10) NOT NULL,
-`last_run_at` datetime DEFAULT NULL,
-`next_run_at` datetime DEFAULT NULL,
-`is_active` tinyint(1) NOT NULL DEFAULT '1'
+CREATE TABLE `ci_cron_job` (
+`id_ci_cy` int(5) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`cicj_name` varchar(100) DEFAULT NULL,
+`cicj_command` varchar(255) NOT NULL,
+`cicj_interval_sec` int(10) NOT NULL,
+`cicj_last_run_at` datetime DEFAULT NULL,
+`cicj_next_run_at` datetime DEFAULT NULL,
+`cicj_is_active` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
