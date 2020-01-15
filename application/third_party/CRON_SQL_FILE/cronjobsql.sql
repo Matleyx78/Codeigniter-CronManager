db5cbb6088604b3e06172bc538761e0efb79dba2
Matleyx78 <matmat78@hotmail.it>
2020-01-15 14:23:42 +0100

    Update cronjobsql.sql

--- application/third_party/CRON_SQL_FILE/cronjobsql.sql
+++ application/third_party/CRON_SQL_FILE/cronjobsql.sql
@@ -1,7 +1,12 @@
 CREATE TABLE `ci_cron_job` (
-`id_ci_cy` int(5) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`id_ci_cj` int(5) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 `cicj_name` varchar(100) DEFAULT NULL,
 `cicj_command` varchar(255) NOT NULL,
+`cicj_minute` varchar(30) NOT NULL,
+`cicj_hour` varchar(30) NOT NULL,
+`cicj_day_of_month` varchar(30) NOT NULL,
+`cicj_month` varchar(30) NOT NULL,
+`cicj_week_day` varchar(30) NOT NULL,
 `cicj_interval_sec` int(10) NOT NULL,
 `cicj_last_run_at` datetime DEFAULT NULL,
 `cicj_next_run_at` datetime DEFAULT NULL,
